55e20381bf7a690a64f3a5d359f82efdc4cf6ae9
Mike Park <14ypark@gmail.com>
2025-10-07 13:42:02 -0400

    add model validations and associations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,16 @@
 class Address < ApplicationRecord
+  has_many :transactions, dependent: :destroy
+  
+  validates :address, presence: true, uniqueness: true
+  # TODO: add validation for btc address format
+  
+  def transaction_count
+    transactions.count
+  end
+  
+  def update_balance!
+    received = transactions.where(tx_type: 'received').sum(:amount)
+    sent = transactions.where(tx_type: 'sent').sum(:amount)
+    update!(balance: received - sent)
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :address
+  
+  validates :tx_hash, presence: true, uniqueness: { scope: :address_id }
+  validates :amount, presence: true, numericality: true
+  validates :tx_type, presence: true, inclusion: { in: ["received", "sent"] }
+  validates :timestamp, presence: true
+  
+  scope :recent, -> { order(timestamp: :desc) }
 end
